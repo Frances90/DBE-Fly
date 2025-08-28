e61c6bc3e8db1611cbd1ff75f201dea39c951bb4
Shadowlein <francesschmidt@hotmail.de>
2025-08-28 15:29:31 +0200

    Flugdaten Design umgesetzt

--- js/main.js
+++ js/main.js
@@ -10,6 +10,43 @@ function openBuchung(id) {
 
 const elementAnzahl = document.getElementById("flug_anzahl");
 const elementNonstop = document.getElementById("flug_nonstop");
+var btbSuche = document.getElementById("btn_suche");
+var flugVon = document.getElementById("input_von");
+var flugNach = document.getElementById("input_nach");
+var flugdaten;
+
+function jsonAuslesen () {
+    if(window.XMLHttpRequest){
+    var request = new XMLHttpRequest();
+    request.open("GET", "./json/flugdaten.json");
+            
+    request.onreadystatechange = function(){
+        if(request.readyState == 4 && request.status == 200){
+                     flugdaten = JSON.parse(request.responseText);
+                    console.log(flugdaten);
+                    /* document.getElementById("textinc").innerHTML = daten.name; */
+        }
+            }
+            request.send();
+
+        }else{
+            alert("Warnung: XMLHttpRequest nicht vorhanden!");
+        }
+}
+
+jsonAuslesen();
+
+btbSuche.addEventListener("click", function(){
+    var vonWert = flugVon.value;
+    var nachWert = flugNach.value
+    console.log("Flug von: " + vonWert + " Flug nach: " + nachWert);
+
+    for(let i = 0; i<flugdaten.length; i++)
+        console.log(flugdaten.length + " " + i);
+});
+
+
+
 
 function windowSize(){
     if (window.innerWidth <= 629) {
@@ -29,10 +66,7 @@ function windowSize(){
 
 windowSize();
 
-
 window.addEventListener("resize", function(){
     windowSize();
 });
-
-
 

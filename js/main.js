c28ea155338225c1adc11c909e56c0066f1217b8
Shadowlein <francesschmidt@hotmail.de>
2025-08-28 16:33:53 +0200

    Flugdaten automatisch anhand der Suche ausfüllen

--- js/main.js
+++ js/main.js
@@ -13,6 +13,8 @@ const elementNonstop = document.getElementById("flug_nonstop");
 var btbSuche = document.getElementById("btn_suche");
 var flugVon = document.getElementById("input_von");
 var flugNach = document.getElementById("input_nach");
+var flugListe = document.querySelector("#container_flugdaten");
+var checkbox = document.getElementById("nonstop");
 var flugdaten;
 
 function jsonAuslesen () {
@@ -40,9 +42,78 @@ btbSuche.addEventListener("click", function(){
     var vonWert = flugVon.value;
     var nachWert = flugNach.value
     console.log("Flug von: " + vonWert + " Flug nach: " + nachWert);
+    flugListe.innerHTML = "";
+    console.log("Checkboxwert: " + checkbox.checked);
+    for(let i = 0; i<flugdaten.length; i++){
+        
+        let flugStart = flugdaten[i].start;
+        let flugZiel = flugdaten[i].ziel;
 
-    for(let i = 0; i<flugdaten.length; i++)
-        console.log(flugdaten.length + " " + i);
+        let flugStartKlein = flugStart.toLowerCase();
+        let flugZielKlein = flugZiel.toLowerCase();
+
+        if(flugStartKlein.includes(vonWert.toLowerCase()) && flugZielKlein.includes(nachWert.toLowerCase()) ){
+            
+            flugListe.innerHTML += `<div id="flugdaten">
+                                    <div class="fluginfo">
+                                        <div id="containerInfo">
+                                            <div class="flugzeit">
+                                                <strong>10:10</strong><br>
+                                                <span>${flugdaten[i].start}</span>
+                                            </div>
+                                            
+                                            <div class="flugdetails">
+                                                <div class="line"></div>
+                                                <div id="design_stops">
+                                                <span>${flugdaten[i].stops}<br>Stopp</span>
+                                                </div>
+                                            </div>
+                                            
+                                            <div class="flug_ankunft">
+                                                <strong>12:45</strong><br>
+                                                <span>${flugdaten[i].ziel}</span><br>
+                                                <small>${flugdaten[i].terminal}</small>
+                                            </div>
+                                        </div>
+                                        <div class="flug_dauer">
+                                                <img src="/resources/clock-outline.svg" alt="Uhr" class="icon" />
+                                                ${flugdaten[i].flugdauer}
+                                        </div>
+                                    </div>
+
+                                    <div class="flug_preise">
+                                        <div class="preis_optionen" id="eco">
+                                            <span class="preisstufe">Economy</span>
+                                            <strong>ab 
+                                                <div class="preis">${flugdaten[i].preis.economy}</div>
+                                            </strong>
+                                            <span></span>
+                                            <div class="dropdown-arrow">
+                                                <img src="/resources/chevron-down.svg" alt="" id="chevron">
+                                            </div>
+                                        </div>
+                                        <div class="preis_optionen" id="business">
+                                            <span class="preisstufe">Business</span>
+                                            <strong>ab 
+                                                <div class="preis">${flugdaten[i].preis.business}</div>
+                                            </strong>
+                                            <span></span>
+                                            <div class="dropdown-arrow">
+                                                <img src="/resources/chevron-down.svg" alt="" id="chevron">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>`;
+        }
+        
+
+
+
+
+
+
+        console.log(flugdaten[i].ziel);
+    }
 });
 
 
